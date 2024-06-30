74dbc7a01478930a21390a9aec2ca6c5ef89b3d5
robatist <rodrigobatista836@gmail.com>
2024-06-30 18:26:15 +0100

    angular version updated to the latest and ng-bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // Import FormsModule for reactive forms
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { ExplicationListComponent } from './explication-list/explication-list.component';
 import { ExplicationDetailsComponent } from './explication-details/explication-details.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ExplicationListComponent,
-    ExplicationDetailsComponent
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    AppRoutingModule, 
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        ExplicationListComponent,
+        ExplicationDetailsComponent
+    ],
+    bootstrap: [AppComponent], imports: [FormsModule,
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
